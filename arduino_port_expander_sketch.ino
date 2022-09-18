38164a6bb3066f3398762ab606938e7d9fc935f6
Mirko De Angelis <theanch92@gmail.com>
2022-09-18 21:38:49 +0200

    Update arduino_port_expander_sketch.ino

--- arduino_port_expander_sketch.ino
+++ arduino_port_expander_sketch.ino
@@ -11,34 +11,29 @@ Ports:
 
 #include <Arduino.h>
 #include <Wire.h>
-#include <DHT.h>
+#include "EmonLib.h"  // Include Emon Library
+EnergyMonitor emon1;
+EnergyMonitor emon2;
+EnergyMonitor emon3;
 
-#define DEBUG // remove debug so pin 0 and 1 can be used for IO
-#define DEBUG_READ // more advanced debuging
+#define DEBUG       // remove debug so pin 0 and 1 can be used for IO
+//#define DEBUG_READ  // more advanced debuging
 
-#define I2C_ADDRESS 8  //i2c starts at pin 8
+#define I2C_ADDRESS 8  // i2c starts at pin 8
 
 int BASE_PIN = 3;
 int NUM_RELAYS = 8;
 
-#define DHT_PIN 11
-
-#define DHT_TYPE DHT22   // DHT 22  (AM2302), AM2321
-
-DHT dht(DHT_PIN, DHT_TYPE);
-
-
 unsigned long previousTemperatureMillis = 0;
 const long temperatureInterval = 5000;
 
-
 void onRequest();
 void onReceive(int);
+void readPower();
 
-void setup()
-{
-  for(int i = BASE_PIN; i < BASE_PIN + NUM_RELAYS; i++) {
-    pinMode(i, OUTPUT); 
+void setup() {
+  for (int i = BASE_PIN; i < BASE_PIN + NUM_RELAYS; i++) {
+    pinMode(i, OUTPUT);
   }
 #ifdef DEBUG
   Serial.begin(115200);
@@ -46,10 +41,10 @@ void setup()
 #endif
 
 #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-    analogReference(INTERNAL1V1);
+  analogReference(INTERNAL1V1);
 #endif
 #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
-    analogReference(INTERNAL);
+  analogReference(INTERNAL);
 #endif
 
   Wire.begin(I2C_ADDRESS);
@@ -60,64 +55,56 @@ void setup()
   Serial.println(F("Wire ok"));
 #endif
 
-  dht.begin();
-#ifdef DEBUG
-  Serial.println(F("DHT ok"));
-#endif
+  // analogReadResolution(ADC_BITS);
+  emon1.voltage(0, 234.59, 0.9);
+  emon1.current(1, 77.1);
+  emon2.voltage(0, 234.49, 0.9);
+  emon2.current(2, 77.1);
+  emon3.voltage(0, 258.69, 0.9);
+  emon3.current(3, 64.8);
 }
 
-void loop()
-{
-  //int temp = analogRead(A1);
-  //Serial.println(temp);
+void loop() {
   unsigned long currentMillis = millis();
-  if (previousTemperatureMillis == 0 || (currentMillis - previousTemperatureMillis >= temperatureInterval)) {
-    Serial.println("Logging temp");
+  if (previousTemperatureMillis == 0 ||
+      (currentMillis - previousTemperatureMillis >= temperatureInterval)) {    
     previousTemperatureMillis = currentMillis;
-    readDht22();
+    readPower();
   }
 }
 
-void printFloatTemplate(const char *pfx, float val, const char *sfx) 
-{
+void printFloatTemplate(const char *pfx, float val, const char *sfx) {
   Serial.print(*pfx);
   Serial.print(val);
   Serial.println(*sfx);
 }
 
-float temp = 0;
-float humidity = 0;
-float heatIndex = 0;
+float realPower1 = 0;
+float realPower2 = 0;
+float realPower3 = 0;
 
-void readDht22() {
-  float f = dht.readTemperature(true);
-  float h = dht.readHumidity();
-  if (isnan(f) || isnan(h)) {
-    #ifdef DEBUG
-    Serial.println("Failed to read from DHT sensor!");
-    #endif
-    return;
-  }
-  temp = f;
-  humidity = h;
-  heatIndex = dht.computeHeatIndex(f, h);
-  #ifdef DEBUG
-  printFloatTemplate("Temperature", temp, "F");
-  printFloatTemplate("Humidity", humidity, "%");
-  printFloatTemplate("Heat Index", heatIndex, "F");
-  #endif
+void readPower() {
+  emon1.calcVI(20, 100);
+  emon2.calcVI(20, 100);
+  emon3.calcVI(20, 100);
+
+  realPower1 = emon1.realPower;
+  realPower2 = emon2.realPower;
+  realPower3 = emon3.realPower;
+#ifdef DEBUG
+  printFloatTemplate("Power1", realPower1, "W");
+  printFloatTemplate("Power2", realPower2, "W");
+  printFloatTemplate("Power3", realPower3, "W");
+#endif
 }
 
-
 volatile byte buffer[9];
-volatile byte len = 1;
+volatile byte len = 9;
 
 #define DIGITAL_READ(b, pin, mask) \
-  if (digitalRead(pin))            \
-    buffer[b] |= mask;
+  if (digitalRead(pin)) buffer[b] |= mask;
 
-void readDigital()
-{
+void readDigital() {
   len = 9;
   buffer[0] = 0;
   buffer[1] = 0;
@@ -128,101 +115,101 @@ void readDigital()
   buffer[6] = 0;
   buffer[7] = 0;
   buffer[8] = 0;
-  
-  DIGITAL_READ(0, 0, 1); //0
-  DIGITAL_READ(0, 1, 2); //1
-  DIGITAL_READ(0, 2, 4); //2
-  DIGITAL_READ(0, 3, 8); //3
-  DIGITAL_READ(0, 4, 16); //4
-  DIGITAL_READ(0, 5, 32); //5
-  DIGITAL_READ(0, 6, 64); //6
-  DIGITAL_READ(0, 7, 128); //7
 
-  DIGITAL_READ(1, 8, 1); //8
-  DIGITAL_READ(1, 9, 2); //9
-  DIGITAL_READ(1, 10, 4); //10
-  DIGITAL_READ(1, 11, 8); //11
-  DIGITAL_READ(1, 12, 16); //12
-  DIGITAL_READ(1, 13, 32); //13
+  DIGITAL_READ(0, 0, 1);    // 0
+  DIGITAL_READ(0, 1, 2);    // 1
+  DIGITAL_READ(0, 2, 4);    // 2
+  DIGITAL_READ(0, 3, 8);    // 3
+  DIGITAL_READ(0, 4, 16);   // 4
+  DIGITAL_READ(0, 5, 32);   // 5
+  DIGITAL_READ(0, 6, 64);   // 6
+  DIGITAL_READ(0, 7, 128);  // 7
 
-//Arduino Mega Boards
+  DIGITAL_READ(1, 8, 1);    // 8
+  DIGITAL_READ(1, 9, 2);    // 9
+  DIGITAL_READ(1, 10, 4);   // 10
+  DIGITAL_READ(1, 11, 8);   // 11
+  DIGITAL_READ(1, 12, 16);  // 12
+  DIGITAL_READ(1, 13, 32);  // 13
+
+// Arduino Mega Boards
 #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-  DIGITAL_READ(1, 14, 64); //14
-  DIGITAL_READ(1, 15, 128); //15
+  DIGITAL_READ(1, 14, 64);   // 14
+  DIGITAL_READ(1, 15, 128);  // 15
 
-  DIGITAL_READ(2, 16, 1); //16
-  DIGITAL_READ(2, 17, 2); //17
-  DIGITAL_READ(2, 18, 4); //18
-  DIGITAL_READ(2, 19, 8); //19
-  DIGITAL_READ(2, 19, 16); //20 Not avaliable on Arduino Mega, i2c pin.
-  DIGITAL_READ(2, 19, 32); //21 Not avaliable on Arduino Mega, i2c pin.
-  DIGITAL_READ(2, 22, 64); //22
-  DIGITAL_READ(2, 23, 128); //23
+  DIGITAL_READ(2, 16, 1);    // 16
+  DIGITAL_READ(2, 17, 2);    // 17
+  DIGITAL_READ(2, 18, 4);    // 18
+  DIGITAL_READ(2, 19, 8);    // 19
+  DIGITAL_READ(2, 19, 16);   // 20 Not avaliable on Arduino Mega, i2c pin.
+  DIGITAL_READ(2, 19, 32);   // 21 Not avaliable on Arduino Mega, i2c pin.
+  DIGITAL_READ(2, 22, 64);   // 22
+  DIGITAL_READ(2, 23, 128);  // 23
 
-  DIGITAL_READ(3, 24, 1); //24
-  DIGITAL_READ(3, 25, 2); //25
-  DIGITAL_READ(3, 26, 4); //26
-  DIGITAL_READ(3, 27, 8); //27
-  DIGITAL_READ(3, 28, 16); //28
-  DIGITAL_READ(3, 29, 32); //29
-  DIGITAL_READ(3, 30, 64); //30
-  DIGITAL_READ(3, 31, 128); //31
+  DIGITAL_READ(3, 24, 1);    // 24
+  DIGITAL_READ(3, 25, 2);    // 25
+  DIGITAL_READ(3, 26, 4);    // 26
+  DIGITAL_READ(3, 27, 8);    // 27
+  DIGITAL_READ(3, 28, 16);   // 28
+  DIGITAL_READ(3, 29, 32);   // 29
+  DIGITAL_READ(3, 30, 64);   // 30
+  DIGITAL_READ(3, 31, 128);  // 31
 
-  DIGITAL_READ(4, 32, 1); //32
-  DIGITAL_READ(4, 33, 2); //33
-  DIGITAL_READ(4, 34, 4); //34
-  DIGITAL_READ(4, 35, 8); //35
-  DIGITAL_READ(4, 36, 16); //36
-  DIGITAL_READ(4, 37, 32); //37
-  DIGITAL_READ(4, 38, 64); //38
-  DIGITAL_READ(4, 39, 128); //39
+  DIGITAL_READ(4, 32, 1);    // 32
+  DIGITAL_READ(4, 33, 2);    // 33
+  DIGITAL_READ(4, 34, 4);    // 34
+  DIGITAL_READ(4, 35, 8);    // 35
+  DIGITAL_READ(4, 36, 16);   // 36
+  DIGITAL_READ(4, 37, 32);   // 37
+  DIGITAL_READ(4, 38, 64);   // 38
+  DIGITAL_READ(4, 39, 128);  // 39
 
-  DIGITAL_READ(5, 40, 1); //40
-  DIGITAL_READ(5, 41, 2); //41
-  DIGITAL_READ(5, 42, 4); //42
-  DIGITAL_READ(5, 43, 8); //43
-  DIGITAL_READ(5, 44, 16); //44
-  DIGITAL_READ(5, 45, 32); //45
-  DIGITAL_READ(5, 46, 64); //46
-  DIGITAL_READ(5, 47, 128); //47
+  DIGITAL_READ(5, 40, 1);    // 40
+  DIGITAL_READ(5, 41, 2);    // 41
+  DIGITAL_READ(5, 42, 4);    // 42
+  DIGITAL_READ(5, 43, 8);    // 43
+  DIGITAL_READ(5, 44, 16);   // 44
+  DIGITAL_READ(5, 45, 32);   // 45
+  DIGITAL_READ(5, 46, 64);   // 46
+  DIGITAL_READ(5, 47, 128);  // 47
 
-  DIGITAL_READ(6, 48, 1); //48
-  DIGITAL_READ(6, 49, 2); //49
-  DIGITAL_READ(6, 50, 4); //50
-  DIGITAL_READ(6, 51, 8); //51
-  DIGITAL_READ(6, 52, 16); //52
-  DIGITAL_READ(6, 53, 32); //53
-  DIGITAL_READ(6, A0, 64); //54
-  DIGITAL_READ(6, A1, 128); //55
+  DIGITAL_READ(6, 48, 1);    // 48
+  DIGITAL_READ(6, 49, 2);    // 49
+  DIGITAL_READ(6, 50, 4);    // 50
+  DIGITAL_READ(6, 51, 8);    // 51
+  DIGITAL_READ(6, 52, 16);   // 52
+  DIGITAL_READ(6, 53, 32);   // 53
+  DIGITAL_READ(6, A0, 64);   // 54
+  DIGITAL_READ(6, A1, 128);  // 55
 
-  DIGITAL_READ(7, A2, 1); //56
-  DIGITAL_READ(7, A3, 2); //57
-  DIGITAL_READ(7, A4, 4); //58
-  DIGITAL_READ(7, A5, 8); //59
-  DIGITAL_READ(7, A6, 16); //60
-  DIGITAL_READ(7, A7, 32); //61
-  DIGITAL_READ(7, A8, 64); //62
-  DIGITAL_READ(7, A9, 128); //63
+  DIGITAL_READ(7, A2, 1);    // 56
+  DIGITAL_READ(7, A3, 2);    // 57
+  DIGITAL_READ(7, A4, 4);    // 58
+  DIGITAL_READ(7, A5, 8);    // 59
+  DIGITAL_READ(7, A6, 16);   // 60
+  DIGITAL_READ(7, A7, 32);   // 61
+  DIGITAL_READ(7, A8, 64);   // 62
+  DIGITAL_READ(7, A9, 128);  // 63
 
-  DIGITAL_READ(8, A10, 1); //64
-  DIGITAL_READ(8, A11, 2); //65
-  DIGITAL_READ(8, A12, 4); //66
-  DIGITAL_READ(8, A13, 8); //67
-  DIGITAL_READ(8, A14, 16); //68
-  DIGITAL_READ(8, A15, 32); //69
+  DIGITAL_READ(8, A10, 1);   // 64
+  DIGITAL_READ(8, A11, 2);   // 65
+  DIGITAL_READ(8, A12, 4);   // 66
+  DIGITAL_READ(8, A13, 8);   // 67
+  DIGITAL_READ(8, A14, 16);  // 68
+  DIGITAL_READ(8, A15, 32);  // 69
 #endif
 
 // Arduino Uno Boards
 #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
-  DIGITAL_READ(1, A0, 64); //14
-  DIGITAL_READ(1, A1, 128); //15
+  DIGITAL_READ(1, A0, 64);   // 14
+  DIGITAL_READ(1, A1, 128);  // 15
 
-  DIGITAL_READ(2, A2, 1); //16
-  DIGITAL_READ(2, A3, 2); //17
-  //DIGITAL_READ(2, A4, 4); //Not avaliable on Arduino Uno, i2c pin.
-  //DIGITAL_READ(2, A5, 8); //Not avaliable on Arduino Uno, i2c pin.
-  DIGITAL_READ(2, A6, 16); //20
-  DIGITAL_READ(2, A7, 32); //21
+  DIGITAL_READ(2, A2, 1);  // 16
+  DIGITAL_READ(2, A3, 2);  // 17
+  // DIGITAL_READ(2, A4, 4); //Not avaliable on Arduino Uno, i2c pin.
+  // DIGITAL_READ(2, A5, 8); //Not avaliable on Arduino Uno, i2c pin.
+  DIGITAL_READ(2, A6, 16);  // 20
+  DIGITAL_READ(2, A7, 32);  // 21
 #endif
 
 // DIGITAL READ not supports on A3 .. A7 on Arduino Uno
@@ -247,8 +234,7 @@ void readDigital()
   Serial.println(buffer[8]);
 #endif
 }
-void readAnalog(int pin)
-{
+void readAnalog(int pin) {
   int val = analogRead(A0 + pin);
   len = 2;
   buffer[0] = val & 0xFF;
@@ -260,41 +246,31 @@ void readAnalog(int pin)
 }
 
 void handleTextCommand(uint8_t id) {
-  // do the thing that was sent
-  // write the status to the buffer
-  
   switch (id) {
-    case 1: // getTemp
+    case 1:  // getTemp
     {
-      String h = String(temp, 2);
-      h.getBytes((unsigned char*)(&buffer), 9);
-    }
-    break;
-    case 2: // getHumidity
+      String h = String(realPower1, 2);
+      h.getBytes((unsigned char *)(&buffer), 9);
+    } break;
+    case 2:  // getHumidity
     {
-      String hu = String(humidity, 2);
-      hu.getBytes((unsigned char*)(&buffer), 9);
-    }
-    break;
-    case 3: // getHeatIndex
+      String hu = String(realPower2, 2);
+      hu.getBytes((unsigned char *)(&buffer), 9);
+    } break;
+    case 3:  // getHeatIndex
     {
-      String hi = String(heatIndex, 2);
-      hi.getBytes((unsigned char*)(&buffer), 9);
-    }
-    break;
+      String hi = String(realPower3, 2);
+      hi.getBytes((unsigned char *)(&buffer), 9);
+    } break;
   }
-  
-  
+
 #ifdef DEBUG_TEXT
   Serial.print(F("Handle Text Command "));
   Serial.println(id);
 #endif
 }
 
-void onRequest()
-{
-  Wire.write(const_cast<uint8_t *>(buffer), len);
-}
+void onRequest() { Wire.write(const_cast<uint8_t *>(buffer), len);}
 
 #define CMD_DIGITAL_READ 0x0
 
@@ -308,110 +284,110 @@ void onRequest()
 
 // 8 analog registers.. A0 to A14
 // A4 and A5 on Arduino Uno are not supported due to I2C
-#define CMD_ANALOG_READ_A0 0b1000 // 0x8
+#define CMD_ANALOG_READ_A0 0b1000  // 0x8
 // ....
-#define CMD_ANALOG_READ_A14 0b10000 //16
+#define CMD_ANALOG_READ_A14 0b10000  // 16
 
 #define CMD_SETUP_ANALOG_INTERNAL 0x10
 #define CMD_SETUP_ANALOG_DEFAULT 0x12
 
 #define CMD_READ_TEXT 42
 
-void onReceive(int numBytes)
-{
+void onReceive(int numBytes) {
 #ifdef DEBUG_READ
   Serial.print("Received bytes: ");
   Serial.println(numBytes);
 #endif
   int cmd = Wire.read();
 
-  switch (cmd)
-  {
-  case CMD_DIGITAL_READ:
-    readDigital();
-    break;
+#ifdef DEBUG_READ
+  Serial.print("Received bytes dato: ");
+  Serial.println(cmd);
+#endif
+
+  switch (cmd) {
+    case CMD_DIGITAL_READ:
+      readDigital();
+      break;
   }
-  if (cmd >= CMD_READ_TEXT && cmd <= (CMD_READ_TEXT + 32)) // 32 is arbitrary limit
+  if (cmd >= CMD_READ_TEXT &&
+      cmd <= (CMD_READ_TEXT + 32))  // 32 is arbitrary limit
   {
-    handleTextCommand(cmd - CMD_READ_TEXT );
+    handleTextCommand(cmd - CMD_READ_TEXT);
     return;
   }
 
-  if (cmd >= CMD_ANALOG_READ_A0 && cmd <= CMD_ANALOG_READ_A14)
-  {
+  if (cmd >= CMD_ANALOG_READ_A0 && cmd <= CMD_ANALOG_READ_A14) {
     readAnalog(cmd & 0b1110);
     return;
   }
 
   int pin = Wire.read();
 
-  switch (cmd)
-  {
-  case CMD_WRITE_DIGITAL_HIGH:
-  case CMD_WRITE_DIGITAL_LOW:
-  {
-    bool output = cmd == CMD_WRITE_DIGITAL_HIGH;
-    digitalWrite(pin, output);
+  switch (cmd) {
+    case CMD_WRITE_DIGITAL_HIGH:
+    case CMD_WRITE_DIGITAL_LOW: {
+      bool output = cmd == CMD_WRITE_DIGITAL_HIGH;
+      digitalWrite(pin, output);
 #ifdef DEBUG
-    Serial.print(F("Pin "));
-    Serial.print(pin);
-    Serial.println(output ? F(" HIGH") : F(" LOW"));
+      Serial.print(F("Pin "));
+      Serial.print(pin);
+      Serial.println(output ? F(" HIGH") : F(" LOW"));
 #endif
-    break;
-  }
-  case CMD_WRITE_ANALOG:
-  {
-    int val = Wire.read() & (Wire.read() << 8);
-    analogWrite(pin, val);
+      break;
+    }
+    case CMD_WRITE_ANALOG: {
+      int val = Wire.read() & (Wire.read() << 8);
+      analogWrite(pin, val);
 #ifdef DEBUG
-    Serial.print(F("Pin "));
-    Serial.print(pin);
-    Serial.print(F(" Analog write "));
-    Serial.println(val);
+      Serial.print(F("Pin "));
+      Serial.print(pin);
+      Serial.print(F(" Analog write "));
+      Serial.println(val);
 #endif
-    break;
-  }
-  case CMD_SETUP_PIN_OUTPUT:
-    pinMode(pin, OUTPUT);
+      break;
+    }
+    case CMD_SETUP_PIN_OUTPUT:
+      pinMode(pin, OUTPUT);
 #ifdef DEBUG
-    Serial.print(F("Pin "));
-    Serial.print(pin);
-    Serial.println(F(" OUTPUT"));
+      Serial.print(F("Pin "));
+      Serial.print(pin);
+      Serial.println(F(" OUTPUT"));
 #endif
-    break;
-  case CMD_SETUP_PIN_INPUT:
-    pinMode(pin, INPUT);
+      break;
+    case CMD_SETUP_PIN_INPUT:
+      pinMode(pin, INPUT);
 #ifdef DEBUG
-    Serial.print(F("Pin "));
-    Serial.print(pin);
-    Serial.println(F("INPUT"));
+      Serial.print(F("Pin "));
+      Serial.print(pin);
+      Serial.println(F("INPUT"));
 #endif
-    break;
-  case CMD_SETUP_PIN_INPUT_PULLUP:
-    pinMode(pin, INPUT_PULLUP);
+      break;
+    case CMD_SETUP_PIN_INPUT_PULLUP:
+      pinMode(pin, INPUT_PULLUP);
 #ifdef DEBUG
-    Serial.print(F("Pin "));
-    Serial.print(pin);
-    Serial.println(F("INPUT PULLUP"));
+      Serial.print(F("Pin "));
+      Serial.print(pin);
+      Serial.println(F("INPUT PULLUP"));
 #endif
-    break;
-  case CMD_SETUP_ANALOG_INTERNAL:
+      break;
+    case CMD_SETUP_ANALOG_INTERNAL:
 
 #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-    analogReference(INTERNAL1V1);
+      analogReference(INTERNAL1V1);
 #endif
 #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
-    analogReference(INTERNAL);
+      analogReference(INTERNAL);
 #endif
 #ifdef DEBUG
-    Serial.println(F("Analog reference INTERNAL"));
+      Serial.println(F("Analog reference INTERNAL"));
 #endif
-    break;
-  case CMD_SETUP_ANALOG_DEFAULT:
-    analogReference(DEFAULT);
+      break;
+    case CMD_SETUP_ANALOG_DEFAULT:
+      analogReference(DEFAULT);
 #ifdef DEBUG
-    Serial.println(F("Analog reference DEFAULT"));
+      Serial.println(F("Analog reference DEFAULT"));
 #endif
-    break;
+      break;
   }
 }
